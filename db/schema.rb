2332bde2788645b16c458fbb89b72e9ad9cb7f63
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-29 15:21:19 -0500

    run db:migrate with new column in vehicles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_29_195247) do
+ActiveRecord::Schema.define(version: 2021_09_29_201615) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "comment_id", null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_09_29_195247) do
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "year"
   end
 
   add_foreign_key "answers", "comments"
