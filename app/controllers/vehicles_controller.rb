2a1be00dbb2f7a4196c933f5f5811f2ced718e62
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-29 15:06:33 -0500

    add index and show action

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,2 +1,13 @@
 class VehiclesController < ApplicationController
+    def index
+        vehicles = Vehicle.all
+
+        render json: vehicles
+    end
+
+    def show
+        vehicle = Vehicle.find_by(id: params[:id])
+
+        render json: vehicle
+    end
 end
